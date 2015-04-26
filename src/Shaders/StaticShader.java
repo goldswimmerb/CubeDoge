08fdf3d03020c9a317ac152e177ad52330bf09ef
goldswimmerb <goldswimmerbj@gmail.com>
2015-04-26 10:00:40 -0400

    Rotating doge Cube

---
+++ src/Shaders/StaticShader.java
@@ -0,0 +1,49 @@
+package Shaders;
+
+import org.lwjgl.util.vector.Matrix4f;
+
+import Entities.Camera;
+import toolbox.Maths;
+
+public class StaticShader extends ShaderProgram{
+	//tells computer where the shaders files are since openGL3+ is an open pipeline
+private static final String VERTEX_FILE = "src/shaders/vertexShader.txt";
+private static final String FRAGMENT_FILE = "src/shaders/fragmentShader.txt";
+	private int location_transformationMatrix;
+	private int location_projectionMatrix;
+	private int location_viewMatrix;
+
+	//loads vertex and fragment shader
+	public StaticShader() {
+		super(VERTEX_FILE, FRAGMENT_FILE);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void bindAttributes() {
+		super.bindAttribute(0, "position");
+		super.bindAttribute(1, "textureCoords");
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	//Gets values from Matrices declared in GSLS
+	protected void getAllUniformLocations() {
+		// TODO Auto-generated method stub
+		location_transformationMatrix = super.getUniformLocation("transformationMatrix");
+		location_projectionMatrix = super.getUniformLocation("projectionMatrix");
+		location_viewMatrix = super.getUniformLocation("viewMatrix");
+	}
+	//loads transformation matrix
+	public void loadTransformationMatrix(Matrix4f matrix){
+		super.loadMatrix(location_transformationMatrix, matrix);
+	}
+	public void loadViewMatrix(Camera camera){
+		Matrix4f viewMatrix = Maths.createViewMatrix(camera);
+		super.loadMatrix(location_viewMatrix, viewMatrix);
+	}
+	public void loadProjectionMatrix(Matrix4f projection){
+		super.loadMatrix(location_projectionMatrix, projection);
+	}
+}
---
+++ src/engineTester/MainGameLoop.java
@@ -0,0 +1,159 @@
+package engineTester;
+import models.RawModel;
+import models.TexturedModel;
+
+import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
+
+import Entities.Camera;
+import Entities.Entity;
+import Shaders.StaticShader;
+import Textures.ModelTexture;
+import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.Renderer;
+public class MainGameLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DisplayManager.createDisplay();
+		Loader loader = new Loader();
+		
+		StaticShader shader = new StaticShader();
+		Renderer renderer = new Renderer(shader);
+		
+		/* these are for a 2d rectange
+		float[] vertices = { 
+				//Left bottom Triangle
+				-0.5f, 0.5f, 0f,//v0 
+				-0.5f, -0.5f, 0f, //v1
+				0.5f, -0.5f, 0f, //v2
+				//Right top triangle
+				//0.5f, -0.5f, 0f,//v1
+				0.5f, 0.5f,0f,//v2
+				//-0.5f, 0.5f, 0f//v3
+		};
+		int[] indicies = {
+				0, 1, 3,
+				3, 1, 2
+		};
+		float[] textureCoords ={
+				0,0, //V0
+				0,1, //V1
+				1,1, //V2
+				1,0 //V3
+		};
+		*/
+		// this is a cube
+		float[] vertices = {			
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,0.5f,-0.5f,		
+				
+				-0.5f,0.5f,0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				0.5f,0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				-0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,0.5f,
+				-0.5f,0.5f,-0.5f,
+				0.5f,0.5f,-0.5f,
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,-0.5f,0.5f,
+				-0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,0.5f
+				
+		};
+		
+		float[] textureCoords = {
+				
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0
+
+				
+		};
+		
+		int[] indices = {
+				0,1,3,	
+				3,1,2,	
+				4,5,7,
+				7,5,6,
+				8,9,11,
+				11,9,10,
+				12,13,15,
+				15,13,14,	
+				16,17,19,
+				19,17,18,
+				20,21,23,
+				23,21,22
+
+		};
+		
+		RawModel model = loader.loadtoVAO(vertices, textureCoords, indices);
+		TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("doge-600")));
+		//TexturedModel staticModel = new TexturedModel(model, texture);
+		
+		Entity entity = new Entity(staticModel, new Vector3f(0,0,-5),0,0,0,1);
+		Camera camera = new Camera();
+		
+		
+		while(!Display.isCloseRequested()){
+			//sets change in position x, y, z
+			entity.increaseRotation(1, 1, 0);
+			//entity.increasePosition(0, 0, -0.1f);
+			camera.move();
+			//sets change in rotation x, y, z
+			//entity.increaseRotation(0, 0, 0);
+			//prepares the renderer to "draw" the program
+			renderer.prepare();
+			//starts the shaders
+			shader.start();
+			shader.loadViewMatrix(camera);
+		//game logic
+			//tells renderer what to render
+		renderer.render(entity,shader);
+		//stops the shaders
+		shader.stop();
+		DisplayManager.updateDisplay();
+
+	}
+		shader.cleanUp();
+		loader.cleanUP();
+	DisplayManager.closeDisplay();
+	}
+}
